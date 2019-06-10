409cc214bb9b8381850280517e26e78872bc76d1
Nitin Pal <nitinluck0123@gmail.com>
2019-06-10 16:16:59 +0530

    implementation of RegistrationActivity

--- app/src/main/java/nitin/luckyproject/shopitout/HomeActivity.java
+++ app/src/main/java/nitin/luckyproject/shopitout/HomeActivity.java
@@ -3,11 +3,11 @@ package nitin.luckyproject.shopitout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity_registration extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        setContentView(R.layout.activity_homeactivity);
     }
 }
---
+++ app/src/main/java/nitin/luckyproject/shopitout/RegistrationActivity.java
@@ -0,0 +1,87 @@
+package nitin.luckyproject.shopitout;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+
+    private EditText email;
+    private EditText pass;
+    private Button signup;
+    private TextView signin;
+    private FirebaseAuth mauth;
+    private ProgressDialog mDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        email = findViewById(R.id.email_reg);
+        pass =  findViewById(R.id.reg_password);
+        signup =findViewById(R.id.btn_sign_up);
+        signin =findViewById(R.id.sign_in);
+
+         mauth = FirebaseAuth.getInstance();
+         mDialog =new ProgressDialog(this);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mEmail =email.getText().toString().trim();
+                String mPass =pass.getText().toString().trim();
+                if(TextUtils.isEmpty(mEmail)){
+                    email.setError("Required Field..");
+                    return;
+                }
+                if(TextUtils.isEmpty(mPass)){
+                    pass.setError("Required Field..");
+                    return;
+                }
+
+                mDialog.setMessage("Processing ..");
+                mDialog.show();
+
+                mauth.createUserWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            Toast.makeText(RegistrationActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                        mDialog.dismiss();
+                        }
+                        else{
+                            Toast.makeText(RegistrationActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        mDialog.dismiss();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+
+
+    }
+}
